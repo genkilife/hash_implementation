c4f4a584abd9eee9f3298a91263b54043d9156d7
genkilife <wangyenkai@gmail.com>
2019-05-22 11:04:39 -0700

    bring up basic class prototype

--- include/basicHash.h
+++ include/basicHash.h
@@ -1,4 +1,16 @@
 #ifndef _BASIC_HASH_H_
 #define _BASIC_HASH_H_
 
+#include "test_util.h"
+
+class basicHash{
+	public:
+		basicHash();
+		~basicHash();
+	// hash function
+	public:
+		virtual RC create();
+		virtual RC insert();
+		virtual RC search();
+};
 #endif
--- src/basicHash.cpp
+++ src/basicHash.cpp
@@ -1,2 +1,19 @@
 #include "basicHash.h"
 
+basicHash::basicHash(){
+}
+
+basicHash::~basicHash(){
+}
+
+RC basicHash::create(){
+	return SUCCESS;
+}
+
+RC basicHash::insert(){
+	return SUCCESS;
+}
+
+RC basicHash::search(){
+	return SUCCESS;
+}
