5cb035c20b98319f8d2ef4de91dcc1e8cb290f3a
genkilife <wangyenkai@gmail.com>
2019-05-23 14:40:14 -0700

    add delete test for chained

---
+++ test/test_chainedHash_05.cpp
@@ -0,0 +1,62 @@
+/*
+This test case generate large keys and insert into hash structure.
+Then, delete keys from the set.
+*/
+#include <vector>
+#include <algorithm>
+#include "test_util.h"
+#include "ChainedHash.h"
+
+using namespace std;
+
+int main(){
+	ChainedHash* chPtr = new ChainedHash();
+	chPtr->printHashMethod();
+
+	/*
+		Generate distinct keys.
+	*/
+	unsigned int keySize = MODPRIME * 0.8;
+	KeyType* keys = new KeyType[keySize];
+
+	genDistinctKeys(keys, keySize);
+
+	/*
+		Initialize hash and insert keys
+	*/
+	chPtr->create();
+	for(unsigned int idx=0; idx < keySize; idx++){
+		if(chPtr->insert(keys[idx], keys[idx]) != SUCCESS){
+			assert(false);
+		}
+		if(chPtr->search(keys[idx]) != SUCCESS){
+			cout<<"bug1 "<<idx<<endl;
+			assert(false);
+		}
+		if(chPtr->search(keys[0]) != SUCCESS){
+			cout<<"bug3 "<<0<<endl;
+		}
+	}
+	cout<<"Finish chained hash insertion "<<keySize <<" elements"<<endl;
+
+	vector<KeyType> shuffledKeys_v(keys, keys+keySize);
+	random_shuffle(shuffledKeys_v.begin(), shuffledKeys_v.end());
+
+	KeyType* shuffledKeys = new KeyType[keySize];
+	for(unsigned int idx=0; idx < keySize; idx++){
+		shuffledKeys[idx] = shuffledKeys_v[idx];
+	}
+	
+	// Test deleteion
+	for(unsigned int idx=0; idx < keySize; idx++){
+		if(chPtr->delkey(keys[idx]) != SUCCESS){
+			assert(false);
+		}
+	}
+	cout<<"Finish chaind hash deletion "<<keySize <<" elements"<<endl;
+	assert(chPtr->isEmptyHash());
+
+	delete [] keys;
+	delete [] shuffledKeys;
+	return 0;
+}
