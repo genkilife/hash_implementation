30c2b9307c3b0b29f561425a1493c2fea6322082
genkilife <wangyenkai@gmail.com>
2019-05-23 00:18:20 -0700

    finish Double hashing

---
+++ include/DoubleHash.h
@@ -0,0 +1,29 @@
+#ifndef _DOUBLE_HASH_H_
+#define _DOUBLE_HASH_H_
+
+#include <iostream>
+#include <cassert>
+#include "test_util.h"
+#include "BasicHash.h"
+
+class DoubleHash: public BasicHash{
+	public:
+		DoubleHash();
+		~DoubleHash();
+	// hash function
+	public:
+		RC create();
+		RC insert(KeyType, ValType);
+		RC search(KeyType);
+		RC delkey(KeyType);
+		RC printHashMethod();
+
+	private:
+		KeyType hashFunc0(KeyType);
+		KeyType hashFunc1(KeyType);
+	private:
+		Bucket* buckets;
+		unsigned int bucketSize;
+		unsigned int keyNum;
+};
+#endif
---
+++ src/DoubleHash.cpp
@@ -0,0 +1,110 @@
+#include "DoubleHash.h"
+
+DoubleHash::DoubleHash(){
+	buckets = NULL;
+}
+
+DoubleHash::~DoubleHash(){
+	if(buckets != NULL){
+		delete [] buckets;
+	}
+}
+
+RC DoubleHash::create(){
+
+	bucketSize = MODPRIME;
+	keyNum = 0;
+
+	if(buckets == NULL){
+		buckets = new Bucket[bucketSize];
+	}
+
+	// Clear valid flag
+	for(unsigned int id=0; id < bucketSize; id++){
+		buckets[id].valid = false;
+	}
+	
+	return SUCCESS;
+}
+
+RC DoubleHash::insert(KeyType key, ValType val){
+
+	// We assume no resize is needed
+	if(keyNum == bucketSize){
+		return -1;
+	}
+
+	#ifdef _ASSERTION_
+		assert(keyNum < bucketSize);
+	#endif
+
+	KeyType hashedId  = hashFunc0(key);
+	KeyType hashedId1 = hashFunc1(key);
+
+	while(buckets[hashedId].valid == true){
+		hashedId = (hashedId + hashedId1) % bucketSize;
+	}
+
+	buckets[hashedId].valid = true;
+	buckets[hashedId].key   = key;
+	buckets[hashedId].value = val;
+	
+	keyNum++;
+	return SUCCESS;
+}
+
+RC DoubleHash::search(KeyType key){
+
+	KeyType hashedId  = hashFunc0(key);
+	KeyType hashedId1 = hashFunc1(key);
+
+	#ifdef _ASSERTION_
+		assert(hashedId  < bucketSize);
+		assert(hashedId1 < bucketSize);
+	#endif
+	
+	while(buckets[hashedId].valid){
+		if(buckets[hashedId].key == key){
+			return SUCCESS;
+		}
+		hashedId = (hashedId + hashedId1) % bucketSize;
+	}
+
+	return -1;
+}
+
+RC DoubleHash::delkey(KeyType key){
+	
+	KeyType hashedId  = hashFunc0(key);
+	KeyType hashedId1 = hashFunc1(key);
+
+	#ifdef _ASSERTION_
+		assert(hashedId  < bucketSize);
+		assert(hashedId1 < bucketSize);
+	#endif
+
+	while(buckets[hashedId].valid){
+		if(buckets[hashedId].key == key){
+			buckets[hashedId].valid = false;
+			keyNum--;
+			return SUCCESS;
+		}
+		hashedId = (hashedId + hashedId1) % bucketSize;
+	}
+
+	return -1;
+}
+
+RC DoubleHash::printHashMethod(){
+	std::cout<<"Double hash class is used!"<<std::endl;
+	return SUCCESS;
+}
+
+KeyType DoubleHash::hashFunc0(KeyType key){
+	return (key>>15) % bucketSize;
+}
+
+KeyType DoubleHash::hashFunc1(KeyType key){
+	return key % bucketSize;
+}
+
