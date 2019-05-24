cdbf2ef92a686644fe4033861d8d804df072b96e
genkilife <wangyenkai@gmail.com>
2019-05-23 22:55:40 -0700

    dump images

--- result/plot_result.py
+++ result/plot_result.py
@@ -26,12 +26,68 @@ with open('result/doubleHash_uniform_insert_timestamp.txt') as f:
 	doubleXasis = [float(x[0])/keySize for x in data[offset:]]
 	doubleYasis = [x[1] for x in data[offset:]]
 
+plt.ylabel('Time per 200 operation(ms)')
+plt.xlabel('loading factor')
 plt.plot(linearXasis, linearYasis, '-', color='red')
 plt.plot(chainedXasis, chainedYasis, '-', color='green')
 plt.plot(cuckooXasis, cuckooYasis, '-', color='#ff7f0eff')
 plt.plot(doubleXasis, doubleYasis, '-', color='#1f77b4ff')
-
-plt.ylabel('Time per 200 operation')
-plt.xlabel('loading factor')
 plt.legend(['linear hashing', 'chained hashing', 'cuckoo hashing', 'double hashing'], loc='upper left')
+plt.savefig('uniform_insert.png')
+plt.show()
+
+plt.cla()
+with open('result/linearHash_uniform_search_timestamp.txt') as f:
+	data = [x.split(' ',1) for x in f.read().splitlines()]
+	linearXasis = [float(x[0])/keySize for x in data[offset:]]
+	linearYasis = [x[1] for x in data[offset:]]
+with open('result/chainedHash_uniform_search_timestamp.txt') as f:
+	data = [x.split(' ',1) for x in f.read().splitlines()]
+	chainedXasis = [float(x[0])/keySize for x in data[offset:]]
+	chainedYasis = [x[1] for x in data[offset:]]
+with open('result/cuckooHash_uniform_search_timestamp.txt') as f:
+	data = [x.split(' ',1) for x in f.read().splitlines()]
+	cuckooXasis = [float(x[0])/keySize for x in data[offset:]]
+	cuckooYasis = [x[1] for x in data[offset:]]
+with open('result/doubleHash_uniform_search_timestamp.txt') as f:
+	data = [x.split(' ',1) for x in f.read().splitlines()]
+	doubleXasis = [float(x[0])/keySize for x in data[offset:]]
+	doubleYasis = [x[1] for x in data[offset:]]
+
+plt.ylabel('Time per 200 operation(ms)')
+plt.xlabel('loading factor')
+plt.plot(linearXasis, linearYasis, '-', color='red')
+plt.plot(chainedXasis, chainedYasis, '-', color='green')
+plt.plot(cuckooXasis, cuckooYasis, '-', color='#ff7f0eff')
+plt.plot(doubleXasis, doubleYasis, '-', color='#1f77b4ff')
+plt.legend(['linear hashing', 'chained hashing', 'cuckoo hashing', 'double hashing'], loc='upper left')
+plt.savefig('uniform_search.png')
+plt.show()
+
+plt.cla()
+with open('result/linearHash_uniform_delete_timestamp.txt') as f:
+	data = [x.split(' ',1) for x in f.read().splitlines()]
+	linearXasis = [float(x[0])/keySize for x in data[offset:]]
+	linearYasis = [x[1] for x in data[offset:]]
+with open('result/chainedHash_uniform_delete_timestamp.txt') as f:
+	data = [x.split(' ',1) for x in f.read().splitlines()]
+	chainedXasis = [float(x[0])/keySize for x in data[offset:]]
+	chainedYasis = [x[1] for x in data[offset:]]
+with open('result/cuckooHash_uniform_delete_timestamp.txt') as f:
+	data = [x.split(' ',1) for x in f.read().splitlines()]
+	cuckooXasis = [float(x[0])/keySize for x in data[offset:]]
+	cuckooYasis = [x[1] for x in data[offset:]]
+with open('result/doubleHash_uniform_delete_timestamp.txt') as f:
+	data = [x.split(' ',1) for x in f.read().splitlines()]
+	doubleXasis = [float(x[0])/keySize for x in data[offset:]]
+	doubleYasis = [x[1] for x in data[offset:]]
+
+plt.ylabel('Time per 200 operation(ms)')
+plt.xlabel('loading factor')
+plt.plot(linearXasis, linearYasis, '-', color='red')
+plt.plot(chainedXasis, chainedYasis, '-', color='green')
+plt.plot(cuckooXasis, cuckooYasis, '-', color='#ff7f0eff')
+plt.plot(doubleXasis, doubleYasis, '-', color='#1f77b4ff')
+plt.legend(['linear hashing', 'chained hashing', 'cuckoo hashing', 'double hashing'], loc='upper left')
+plt.savefig('uniform_delete.png')
 plt.show()
